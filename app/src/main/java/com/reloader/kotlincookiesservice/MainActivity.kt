342c77632c84e19ad5b3e1784903392296afbe80
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-04-28 18:38:55 -0500

    Share Preference

--- app/src/main/java/com/reloader/kotlincookiesservice/MainActivity.kt
+++ app/src/main/java/com/reloader/kotlincookiesservice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.reloader.kotlincookiesservice
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.reloader.kotlincookiesservice.Utils.ShareDataUtil
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -10,9 +11,12 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPreference = ShareDataUtil(this)
+       val data = sharedPreference.save("nombre", "Reloader System")
 
         val endPoint = getConfiguration().create(APIService::class.java)
         val responseBodyCall = endPoint.postRegistrarUsuario("a@a.com", "123456")
